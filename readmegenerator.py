2302a2e95ad86cf2da2b17deb4de4c0f18d89b92
HarshitPal25 <harshit13082006@gmail.com>
2025-09-16 12:45:41 +0530

    succes

--- readmegenerator.py
+++ readmegenerator.py
@@ -6,16 +6,16 @@ def generate_readme():
     linkdin = input("Enter you linkdin URL: ")
 
     readme = f"""
-        # Hi There 👋, I am {name}
-        [![Visitors](https://visitor-badge.laobi.icu/badge?page_id={github_username})]
-        ## 🚀 About Me
-        {bio}
-        ## 🛠️ Skills
-        {", ".join(skill.strip() for skill in skills.split(","))}
-        ## 📊 GitHub Stats
-        ![GitHub Stats](https://github-readme-stats.vercel.app/api?username={github_username}&show_icons=true&theme=radical)
-        ## 🌐 Connect with me
-        [LinkdIn]({linkdin})
+# Hi There 👋, I am {name}
+[![Visitors](https://visitor-badge.laobi.icu/badge?page_id={github_username})]
+## 🚀 About Me
+{bio}
+## 🛠️ Skills
+{", ".join(skill.strip() for skill in skills.split(","))}
+## 📊 GitHub Stats
+![GitHub Stats](https://github-readme-stats.vercel.app/api?username={github_username}&show_icons=true&theme=radical)
+## 🌐 Connect with me
+[LinkdIn]({linkdin})
         """
     with open("README.md", "w", encoding="utf-8") as f:
         f.write(readme)
